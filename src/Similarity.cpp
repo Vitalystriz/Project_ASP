6db56afecad73f4da30bd380baa290fa584a319c
MikaLevAri <mikalevari1@gmail.com>
2026-05-04 15:09:28 +0300

    documentation

--- src/Similarity.cpp
+++ src/Similarity.cpp
@@ -3,6 +3,8 @@
     void Similarity::calculateSimilarity(std::map<int, std::vector<int>>& matrix, int user) {
         std::vector<int>& userProducts = matrix[user];
 
+        //for all users that are not our target user- calculate the amount of shared products which
+        //will act as the similarity
         std::vector<int>>::iterator it = matrix.begin();
         for (std::map<int, it; it != matrix.end(); ++it) {
             int otherId = it->first;
