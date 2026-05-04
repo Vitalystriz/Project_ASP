2a081f70272050738de99dddd860319af1da6dc5
vitaly <vitalystriz@gmail.com>
2026-05-04 13:14:04 +0300

    Fix: remove IDE and build artifacts from repository and update gitignore

--- cmake-build-debug/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Vitaly/StudyDrive/SecondYear/AdvancedSystemProgramming/Project_ASP/cmake-build-debug/_deps/googletest-src/googletest
-# Build directory: C:/Vitaly/StudyDrive/SecondYear/AdvancedSystemProgramming/Project_ASP/cmake-build-debug/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
